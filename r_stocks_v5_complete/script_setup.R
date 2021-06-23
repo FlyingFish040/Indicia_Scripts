4438de471dbbe7dfab00c31526da03742c804057
stan-gommans <83220100+stan-gommans@users.noreply.github.com>
2021-06-23 23:11:34 +0200

    Script complete

---
+++ r_stocks_v5_complete/script_setup.R
@@ -0,0 +1,3 @@
+
+packages <- c("rvest", "xfun", "tidyverse", "tidytext", "lsa", "lubridate", "googlesheets4", "gargle", "dplyr", "httr", "jsonlite", "httpuv", "rtweet")
+install.packages(packages, repos = "https://cloud.r-project.org")
